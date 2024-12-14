b7a6a9e85bbe71d22392cfc05476643419eff018
Margus Sikk <MargusSikk@hot.ee>
2024-12-14 13:46:04 +0200

    Year 2024, Day 13

---
+++ AdventOfCode/Year2024/Day13/ClawMachine.cs
@@ -0,0 +1,89 @@
+﻿using AdventOfCode.Utilities.Geometry;
+using AdventOfCode.Utilities.Numerics;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Year2024.Day13;
+internal partial class ClawMachine
+{
+    public Vector2D ButtonA { get; set; }
+
+    public Vector2D ButtonB { get; set; }
+
+    public Coordinate2D Prize { get; set; }
+
+    public bool TryWinThePrize(out long buttonAPushes, out long buttonBPushes)
+    {
+        var matrix = new Matrix<double>(2, 3);
+        matrix.SetRow(0, [ButtonA.DX, ButtonB.DX, Prize.X]);
+        matrix.SetRow(1, [ButtonA.DY, ButtonB.DY, Prize.Y]);
+
+        if (!LinearEquationSolver.TrySolveLinearEquation(matrix, out var doubleValues))
+        {
+            buttonAPushes = 0;
+            buttonBPushes = 0;
+
+            return false;
+        }
+
+        buttonAPushes = Convert.ToInt64(doubleValues[0]);
+        buttonBPushes = Convert.ToInt64(doubleValues[1]);
+
+        var verifyA = ButtonA.DX * buttonAPushes + ButtonB.DX * buttonBPushes == Prize.X;
+        var verifyB = ButtonA.DY * buttonAPushes + ButtonB.DY * buttonBPushes == Prize.Y;
+
+        if (!verifyA || !verifyB)
+        {
+            buttonAPushes = 0;
+            buttonBPushes = 0;
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public static ClawMachine Parse(string[] inputLines)
+    {
+        var clawMachine = new ClawMachine();
+
+        if (inputLines.Length != 3)
+        {
+            throw new InvalidOperationException("Incorrect amount of ClawMachine input lines");
+        }
+
+        // Button A
+        var matches = ButtonInputLineRegex().Matches(inputLines[0]);
+        if (matches.Count != 1)
+        {
+            throw new InvalidOperationException("Failed to parse Button A input line");
+        }
+
+        clawMachine.ButtonA = new Vector2D(long.Parse(matches[0].Groups[1].Value), long.Parse(matches[0].Groups[2].Value));
+
+        // Button B
+        matches = ButtonInputLineRegex().Matches(inputLines[1]);
+        if (matches.Count != 1)
+        {
+            throw new InvalidOperationException("Failed to parse Button B input line");
+        }
+
+        clawMachine.ButtonB = new Vector2D(long.Parse(matches[0].Groups[1].Value), long.Parse(matches[0].Groups[2].Value));
+
+        // Prize
+        matches = PrizeInputLineRegex().Matches(inputLines[2]);
+        if (matches.Count != 1)
+        {
+            throw new InvalidOperationException("Failed to parse Prize input line");
+        }
+
+        clawMachine.Prize = new Coordinate2D(long.Parse(matches[0].Groups[1].Value), long.Parse(matches[0].Groups[2].Value));
+
+        return clawMachine;
+    }
+
+    [GeneratedRegex(@"Button [A|B]: X\+(\d+), Y\+(\d+)")]
+    private static partial Regex ButtonInputLineRegex();
+
+    [GeneratedRegex(@"Prize: X=(\d+), Y=(\d+)")]
+    private static partial Regex PrizeInputLineRegex();
+}
---
+++ AdventOfCode/Year2024/Day13/Day13PuzzleSolver.cs
@@ -0,0 +1,49 @@
+using AdventOfCode.Framework.Puzzle;
+using AdventOfCode.Utilities.Extensions;
+using AdventOfCode.Utilities.Geometry;
+
+namespace AdventOfCode.Year2024.Day13;
+
+[Puzzle(2024, 13, "Claw Contraption")]
+public class Day13PuzzleSolver : IPuzzleSolver
+{
+    private List<ClawMachine> _clawMachines = [];
+
+    public void ParseInput(string[] inputLines)
+    {
+        _clawMachines = inputLines.SelectToChunks()
+                                  .Select(ClawMachine.Parse)
+                                  .ToList();        
+    }
+
+    public PuzzleAnswer GetPartOneAnswer()
+    {
+        var answer = _clawMachines
+            .Select(cm => cm.TryWinThePrize(out var numberOfButtonAPushes, out var numberOfButtonBPushes)
+                ? (ButtonAPushes: numberOfButtonAPushes, ButtonBPushes: numberOfButtonBPushes)
+                : (ButtonAPushes: 0L, ButtonBPushes: 0L))
+            .Where(cm => cm.ButtonAPushes <= 100 && cm.ButtonBPushes <= 100)
+            .Sum(x => x.ButtonAPushes * 3 + x.ButtonBPushes);
+
+        return new PuzzleAnswer(answer, 27105);
+    }
+
+    public PuzzleAnswer GetPartTwoAnswer()
+    {
+        const long prizeIncrease = 10_000_000_000_000L;
+
+        var answer = _clawMachines
+            .Select(cm => new ClawMachine
+            {
+                ButtonA = cm.ButtonA,
+                ButtonB = cm.ButtonB,
+                Prize = new Coordinate2D(cm.Prize.X + prizeIncrease, cm.Prize.Y + prizeIncrease),
+            })
+            .Select(cm => cm.TryWinThePrize(out var numberOfButtonAPushes, out var numberOfButtonBPushes)
+                ? (ButtonAPushes: numberOfButtonAPushes, ButtonBPushes: numberOfButtonBPushes)
+                : (ButtonAPushes: 0L, ButtonBPushes: 0L))
+            .Sum(x => x.ButtonAPushes * 3 + x.ButtonBPushes);
+
+        return new PuzzleAnswer(answer, 101726882250942L);
+    }
+}
