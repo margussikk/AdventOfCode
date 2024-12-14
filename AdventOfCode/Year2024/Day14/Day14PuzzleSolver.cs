0131877c2c93387e738094651728a9308c94b6fa
Margus Sikk <MargusSikk@hot.ee>
2024-12-14 16:02:04 +0200

    Year 2024, Day 14

---
+++ AdventOfCode/Year2024/Day14/Day14PuzzleSolver.cs
@@ -0,0 +1,77 @@
+using AdventOfCode.Framework.Puzzle;
+using AdventOfCode.Utilities.Geometry;
+using AdventOfCode.Utilities.Mathematics;
+using System.Globalization;
+
+namespace AdventOfCode.Year2024.Day14;
+
+[Puzzle(2024, 14, "Restroom Redoubt")]
+public class Day14PuzzleSolver : IPuzzleSolver
+{
+    private List<Robot> _robots = [];
+
+    public void ParseInput(string[] inputLines)
+    {
+        _robots = inputLines.Select(Robot.Parse)
+                            .ToList();
+    }
+
+    public PuzzleAnswer GetPartOneAnswer()
+    {
+        const int seconds = 100;
+        const int width = 101;
+        const int height = 103;
+
+        var robotsInQuadrants = new long[4];
+
+        var betweenX = width / 2;
+        var betweenY = height / 2;
+
+        foreach (var robot in _robots)
+        {
+            var x = MathFunctions.Modulo(robot.Position.X + seconds * robot.Velocity.DX, width);
+            var y = MathFunctions.Modulo(robot.Position.Y + seconds * robot.Velocity.DY, height);
+
+            if (x == betweenX || y == betweenY)
+            {
+                continue;
+            }
+            
+            var quadrant = x > betweenX ? 1 : 0;
+            quadrant += y > betweenY ? 2 : 0;
+
+            robotsInQuadrants[quadrant]++;            
+        }
+
+        var answer = robotsInQuadrants.Aggregate(1L, (agg, curr) => agg *  curr);
+
+        return new PuzzleAnswer(answer, 225648864L);
+    }
+
+    public PuzzleAnswer GetPartTwoAnswer()
+    {
+        var robots = _robots
+            .Select(r => r.Clone())
+            .ToList();
+
+        var answer = 0;
+
+        for (int seconds = 1; seconds < int.MaxValue; seconds++)
+        {
+            foreach (var robot in robots)
+            {
+                robot.Move();
+            }
+
+            if (robots.Select(r => r.Position)
+                      .Distinct()
+                      .Count() == robots.Count)
+            {
+                answer = seconds;
+                break;
+            }
+        }
+
+        return new PuzzleAnswer(answer, 7847);
+    }
+}
---
+++ AdventOfCode/Year2024/Day14/Robot.cs
@@ -0,0 +1,53 @@
+﻿using AdventOfCode.Utilities.Geometry;
+using AdventOfCode.Utilities.Mathematics;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Year2024.Day14;
+internal partial class Robot
+{
+    private const long areaWidth = 101;
+    private const long areaHeight = 103;
+
+    public Coordinate2D Position { get; set; }
+    public Vector2D Velocity { get; set; }
+
+    public Robot Clone()
+    {
+        return new Robot
+        {
+            Position = Position,
+            Velocity = Velocity
+        };
+    }
+
+    public void Move()
+    {
+        var x = MathFunctions.Modulo(Position.X + Velocity.DX, areaWidth);
+        var y = MathFunctions.Modulo(Position.Y + Velocity.DY, areaHeight);
+
+        Position = new Coordinate2D(x, y);
+    }
+
+    public static Robot Parse(string input)
+    {
+        var matches = InputLineRegex().Matches(input);
+        if (matches.Count != 1)
+        {
+            throw new InvalidOperationException("Failed to parse input line");
+        }
+
+        var match = matches[0];
+
+        var position = new Coordinate2D(int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value));
+        var velocity = new Vector2D(int.Parse(match.Groups[3].Value), int.Parse(match.Groups[4].Value));
+
+        return new Robot
+        {
+            Position = position,
+            Velocity = velocity
+        };
+    }
+
+    [GeneratedRegex(@"p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)")]
+    private static partial Regex InputLineRegex();
+}
