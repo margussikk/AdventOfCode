db713746379b945167ad09c086af0ec4fb4df417
Margus Sikk <MargusSikk@hot.ee>
2024-12-22 22:14:33 +0200

    Year 2024, Day 21

--- AdventOfCode/Year2024/Day21/KeypadWalker.cs
+++
@@ -1,8 +0,0 @@
-﻿using AdventOfCode.Utilities.Geometry;
-
-namespace AdventOfCode.Year2024.Day21;
-internal class KeypadWalker
-{
-    public GridCoordinate Coordinate { get; set; }
-    public GridCoordinate? PreviousCoordinate { get; set; }
-}
