db189ef336b901c2bcfcbaf04e62842983ce7125
Margus Sikk <MargusSikk@hot.ee>
2025-01-05 09:49:50 +0200

    Year 2018, Day 21

--- AdventOfCode/Year2018/Day21/Day21PuzzleSolver.cs
+++ AdventOfCode/Year2018/Day21/Day21PuzzleSolver.cs
@@ -16,7 +16,9 @@ public class Day21PuzzleSolver : IPuzzleSolver
             throw new InvalidOperationException("First line in input wasn't #ip");
         }
 
-        _instructions = inputLines.Skip(1).Select(Instruction.Parse).ToList();
+        _instructions = inputLines.Skip(1)
+                                  .Select(Instruction.Parse)
+                                  .ToList();
     }
 
     public PuzzleAnswer GetPartOneAnswer()
@@ -33,7 +35,7 @@ public class Day21PuzzleSolver : IPuzzleSolver
         return new PuzzleAnswer(answer, 1413889);
     }
 
-    // This is reverse enginereed algorithm of the program
+    // This is reverse engineered algorithm of the program
     private IEnumerable<long> GenerateRegister0Values()
     {
         var generatedRegister0Values = new HashSet<long>();
@@ -47,8 +49,8 @@ public class Day21PuzzleSolver : IPuzzleSolver
 
         while (true)
         {
-            register2 = register3 | 65536L;       // bori 3 65536 2
-            register3 = seedValue;                // seti 10736359 9 3
+            register2 = register3 | 65536L;      // bori 3 65536 2
+            register3 = seedValue;               // seti 10736359 9 3
             while (true)
             {
                 var register1 = register2 & 255; // bani 2 255 1
@@ -62,7 +64,7 @@ public class Day21PuzzleSolver : IPuzzleSolver
                     break;
                 }
 
-                // Following instructions increase register 1 and register 5 until
+                // The following instructions increase register 1 and register 5 until
                 // they find register2 / 256
                 // seti 0 3 1, addi 1 1 5, muli 5 256 5, gtrr 5 2 5, addr 5 4 4,
                 // addi 4 1 4, seti 25 8 4, addi 1 1 1, seti 17 6 4, setr 1 5 2
